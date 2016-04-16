41e3300d83e980ebb1249137d152e6eaa68292dc
Max <burmistrovm@live.ru>
2016-04-16 14:23:33 +0500

    Building the GUI

--- web/views.py
+++ web/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+exams = {
+    'мат': 'математика',
+    'инф': 'информатика',
+    'ино': 'иностранный',
+    'общ': 'обществознание',
+    'гео': 'география',
+    'био': 'биология',
+    'рус': 'русский язык',
+    'ист': 'история',
+}
+
+
 def index(request):
-    return HttpResponse('OK')
+    return render(request, 'base.html', {'exams': exams})
