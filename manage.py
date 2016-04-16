465e110d9f7b3283dd54d7e9e3fdcf2a5d5dead8
Max <burmistrovm@live.ru>
2016-04-16 12:48:45 +0500

    Merging

--- manage.py
+++ manage.py
@@ -3,12 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ivs.settings")
-=======
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ivsperm.settings")
->>>>>>> 9226d2358e5e2f099a18e681b13334ebc842e9b3
-
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
